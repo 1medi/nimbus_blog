97205d0616666129a2b2a92fff34f027acb18187
Trevor <ttan20@my.bcit.ca>
2024-10-14 23:47:30 -0500

    here's my version of the blog, i added our colours as well as our initial pock up design for our app. I made the blog posts about what we are, and what we have to offer, I would like to change the order of the blog posts so we can have "what is Aether" on top, but ill learn how to do that later

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       </head>
       <body
-        className={cn(inter.className, "dark:bg-fuchsia-800 dark:text-stone-200")}
+        className={cn(inter.className, "dark:bg-silver-lake-blue dark:text-stone-200")}
       >
         <ThemeSwitcher />
         <div className="min-h-screen">{children}</div>
