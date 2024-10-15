ffdf07a783cd563cb39778dfd4e96e46dd651499
1medi <144403286+1medi@users.noreply.github.com>
2024-10-15 08:46:48 -0700

    Update post-body.tsx

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -1,17 +1,16 @@
-import markdownStyles from './markdown-styles.module.css';  // CSS Module
-import '../styles.css';  // Global CSS (if needed)
+import markdownStyles from "./markdown-styles.module.css";
 
 type Props = {
   content: string;
-  type: string;  // The post type from frontmatter
 };
 
-export function PostBody({ content, type }: Props) {
+export function PostBody({ content}: Props) {
   return (
-    <div className={`max-w-2xl mx-auto ${type}`}>
-      <div className={markdownStyles.markdown}>
-        <div dangerouslySetInnerHTML={{ __html: content }} />
-      </div>
+    <div className="max-w-2xl mx-auto">
+      <div
+        className={markdownStyles["markdown"]}
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
     </div>
   );
 }
